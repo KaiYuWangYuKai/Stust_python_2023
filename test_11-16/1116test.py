c80895c7b956bdacb2a93d675606f3eff6a62eae
YuKai <112930904+KaiYuWangYuKai@users.noreply.github.com>
2023-11-16 11:36:00 +0800

    Update 1116test.py

--- test_11-16/1116test.py
+++ test_11-16/1116test.py
@@ -1,1 +1,19 @@
-print('Hallo World')
+class person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"Name:{self.name}\n Age:{self.age}"    
+
+    def myfunc(self):
+        print("Hallo my name is :"+self.name)
+
+
+p1 = person("Elsa",269)
+p1.myfunc
+
+print(p1.name)
+print(p1.age)
+
+print(p1)
